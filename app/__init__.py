22c4fa0cf81a7a4b282e4aae16aa9ead6705810b
Gideon-Muriithi <gideongakenge@gmail.com>
2019-09-15 12:12:35 +0300

    This commit adds bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from config import DevConfig
+from flask_bootstrap import Bootstrap
 
 # Initializing application
 app = Flask(__name__, instance_relative_config = True)
 
 # Setting up configuration
 app.config.from_object(DevConfig)
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 
 
 from .main import views
